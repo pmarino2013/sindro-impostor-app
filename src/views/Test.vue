148f01119d5bd6fad5022bda49ef7f413c94e723
Pablo Marino <pmarino2013@gmail.com>
2022-09-20 14:57:36 -0300

    Mejorando diseño de test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,41 +5,43 @@
         <h1>¿Tienes el síndrome del impostor? ¡Descúbrelo aquí!</h1>
       </div>
     </div>
-    <div class="row mt-5">
-      <div
-        v-for="pregunta in test"
-        :key="pregunta.id"
-        class="col-12 col-md-6 mb-3 gap-2"
-      >
-        <div class="d-flex gap-2">
-          <h2 class="text-success">{{ pregunta.id }}</h2>
-          <h5>
-            {{ pregunta.texto }}
-          </h5>
-        </div>
-        <div>
-          <div
-            v-for="option in pregunta.opciones"
-            :key="option.opcion"
-            class="form-check"
-          >
-            <input
-              class="form-check-input"
-              type="radio"
-              :name="pregunta.id"
-              :value="option.opcion"
-              @change="guardarRespuesta"
-            />
-            <label class="form-check-label d-flex gap-2">
-              <h5>{{ option.opcion }}</h5>
-              <span>{{ option.valor }}</span>
-            </label>
+    <div class="row row-cols-1 row-cols-md-2 mt-5 g-4">
+      <div class="col" v-for="pregunta in test" :key="pregunta.id">
+        <div class="card h-100 p-0">
+          <div class="card-header bg-success text-white h-100">
+            <div class="d-flex gap-2">
+              <h2 class="fs-1">{{ pregunta.id }}</h2>
+              <h5 class="m-0">
+                {{ pregunta.texto }}
+              </h5>
+            </div>
+          </div>
+          <div class="card-body">
+            <div
+              v-for="option in pregunta.opciones"
+              :key="option.opcion"
+              class="form-check"
+            >
+              <input
+                class="form-check-input"
+                type="radio"
+                :name="pregunta.id"
+                :value="option.opcion"
+                @change="guardarRespuesta"
+              />
+              <label class="form-check-label d-flex gap-2">
+                <h5>{{ option.opcion }}</h5>
+                <span>{{ option.valor }}</span>
+              </label>
+            </div>
           </div>
         </div>
       </div>
+    </div>
+    <div class="row">
       <div class="d-flex justify-content-end my-3">
         <button
-          class="btn btn-success"
+          class="btn btn-success btn-lg"
           @click="verResultado"
           :disabled="resultados.length < 8"
         >
