926133d0e8bc2327a900b1a07b3e9f6dc05bb852
Pablo Marino <pmarino2013@gmail.com>
2022-09-25 19:44:05 -0300

    Pagina NotFound

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>Not found 😓</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
